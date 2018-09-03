97e2cc0556adb0bac865cc106ebf7c26b18f51a4
TarpeDiem <tarpediem@gmail.com>
2018-09-04 00:00:44 +0700

    Create esp32.ino

---
+++ esp32.ino
@@ -0,0 +1,47 @@
+#define BLYNK_PRINT Serial
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = "YourAuthToken";
+const int sensorPin = 4; 
+int sensorState = 0;
+int lastState = 0;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, "SSID", "PASSWORD");
+  pinMode(sensorPin, INPUT);
+}
+
+void loop()
+{ 
+  Blynk.run();
+
+  sensorState = digitalRead(sensorPin);
+Serial.println(sensorState);
+
+if (sensorState == 1 && lastState == 0) {
+  Serial.println("needs water, send notification");
+  Blynk.notify("Water your plants");
+  lastState = 1;
+  delay(1000);
+//send notification
+    
+  } 
+  else if (sensorState == 1 && lastState == 1) {
+    //do nothing, has not been watered yet
+  Serial.println("has not been watered yet");
+  delay(1000);
+  }
+  else {
+    //st
+    Serial.println("does not need water");
+    lastState = 0;
+    delay(1000);
+  }
+  
+  delay(100);
+}
